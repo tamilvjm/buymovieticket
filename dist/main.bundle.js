262c3ab466e730cc21b68a4db90da1e18cd2f7cd
Tamilselvan N <tamilvjm@users.noreply.github.com>
2017-06-29 23:28:40 +0530

    hot fix for deployment issue

---
+++ dist/main.bundle.js
@@ -0,0 +1,457 @@
+webpackJsonp([1,4],{
+
+/***/ 100:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = __webpack_require__(4);
+var AppComponent = (function () {
+    function AppComponent() {
+        this.title = 'app works!';
+    }
+    return AppComponent;
+}());
+AppComponent = __decorate([
+    core_1.Component({
+        selector: 'app-root',
+        template: __webpack_require__(164),
+        styles: [__webpack_require__(159)]
+    })
+], AppComponent);
+exports.AppComponent = AppComponent;
+//# sourceMappingURL=app.component.js.map
+
+/***/ }),
+
+/***/ 101:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var platform_browser_1 = __webpack_require__(18);
+var core_1 = __webpack_require__(4);
+var forms_1 = __webpack_require__(97);
+var http_1 = __webpack_require__(98);
+var app_component_1 = __webpack_require__(100);
+var navbar_component_1 = __webpack_require__(103);
+var carousel_component_1 = __webpack_require__(63);
+var seatlayout_component_1 = __webpack_require__(64);
+var app_routing_1 = __webpack_require__(102);
+var AppModule = (function () {
+    function AppModule() {
+    }
+    return AppModule;
+}());
+AppModule = __decorate([
+    core_1.NgModule({
+        declarations: [
+            app_component_1.AppComponent,
+            navbar_component_1.NavbarComponent,
+            carousel_component_1.CarouselComponent,
+            seatlayout_component_1.SeatlayoutComponent
+        ],
+        imports: [
+            platform_browser_1.BrowserModule,
+            forms_1.FormsModule,
+            http_1.HttpModule,
+            app_routing_1.Routing
+        ],
+        providers: [],
+        bootstrap: [app_component_1.AppComponent]
+    })
+], AppModule);
+exports.AppModule = AppModule;
+//# sourceMappingURL=app.module.js.map
+
+/***/ }),
+
+/***/ 102:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+var router_1 = __webpack_require__(62);
+var seatlayout_component_1 = __webpack_require__(64);
+var carousel_component_1 = __webpack_require__(63);
+var appRoutes = [
+    {
+        path: 'buyticket',
+        component: seatlayout_component_1.SeatlayoutComponent
+    },
+    {
+        path: '',
+        component: carousel_component_1.CarouselComponent
+    }
+];
+exports.Routing = router_1.RouterModule.forRoot(appRoutes);
+//# sourceMappingURL=app.routing.js.map
+
+/***/ }),
+
+/***/ 103:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = __webpack_require__(4);
+var NavbarComponent = (function () {
+    function NavbarComponent() {
+    }
+    NavbarComponent.prototype.ngOnInit = function () {
+    };
+    return NavbarComponent;
+}());
+NavbarComponent = __decorate([
+    core_1.Component({
+        selector: 'app-navbar',
+        template: __webpack_require__(166),
+        styles: [__webpack_require__(161)]
+    }),
+    __metadata("design:paramtypes", [])
+], NavbarComponent);
+exports.NavbarComponent = NavbarComponent;
+//# sourceMappingURL=navbar.component.js.map
+
+/***/ }),
+
+/***/ 104:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.environment = {
+    production: false
+};
+//# sourceMappingURL=environment.js.map
+
+/***/ }),
+
+/***/ 159:
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(15)(false);
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ 160:
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(15)(false);
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ 161:
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(15)(false);
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ 162:
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(15)(false);
+// imports
+
+
+// module
+exports.push([module.i, "#holder{    \r\nheight:200px;    \r\nwidth:550px;\r\nbackground-color:#F5F5F5;\r\nborder:1px solid #A4A4A4;\r\nmargin-left:10px;   \r\n}\r\n#place {\r\nposition:relative;\r\nmargin:7px;\r\n}\r\n a{\r\nfont-size:0.6em;\r\n}\r\nli\r\n{\r\n list-style: none outside none;\r\n position: absolute;   \r\n}    \r\nli:hover\r\n{\r\nbackground-color:yellow;      \r\n} \r\n.seat{\r\nbackground: mediumseagreen;\r\nheight:62px;\r\nwidth:62px;\r\ndisplay:block;   \r\n}\r\n.selectedSeat\r\n{ \r\nbackground: red;\r\n}\r\n.selectingSeat\r\n{ \r\nbackground: yellow; \r\n}\r\n#place .row-3, #place .row-4{\r\nmargin-top:10px;\r\n}\r\n\r\n#seatDescription li{\r\nverticle-align:middle;    \r\nlist-style: none outside none;\r\npadding-left:35px;\r\nheight:35px;\r\nfloat:left;\r\n}\r\n\r\n.fixedseatcontainer{\r\n    width: 450px;\r\n    overflow: auto;\r\n}", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ 164:
+/***/ (function(module, exports) {
+
+module.exports = "<div class=\"navbar-wrapper\">\n  <div class=\"container\">\n    <app-navbar></app-navbar>\n    <router-outlet></router-outlet>\n  </div>\n</div>\n"
+
+/***/ }),
+
+/***/ 165:
+/***/ (function(module, exports) {
+
+module.exports = "<!--<div class=\"inner cover\">\n  <h1 class=\"cover-heading\">Cover your page.</h1>\n  <p class=\"lead\">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen\n    background photo to make it your own.</p>\n  <p class=\"lead\">\n    <a href=\"#\" class=\"btn btn-lg btn-default\">Learn more</a>\n  </p>\n</div>\n\n<div class=\"mastfoot\">\n  <div class=\"inner\">\n    <p>Cover template for <a href=\"http://getbootstrap.com\">Bootstrap</a>, by <a href=\"https://twitter.com/mdo\">@mdo</a>.</p>\n  </div>\n</div>-->\n\nHome page\nTODO: Adding carousel"
+
+/***/ }),
+
+/***/ 166:
+/***/ (function(module, exports) {
+
+module.exports = "<nav class=\"navbar navbar-inverse navbar-static-top\">\n  <div class=\"container\">\n    <div class=\"navbar-header\">\n      <button type=\"button\" class=\"navbar-toggle collapsed\" data-toggle=\"collapse\" data-target=\"#navbar\" aria-expanded=\"false\"\n        aria-controls=\"navbar\">\n            <span class=\"sr-only\">Toggle navigation</span>\n            <span class=\"icon-bar\"></span>\n            <span class=\"icon-bar\"></span>\n            <span class=\"icon-bar\"></span>\n          </button>\n      <a class=\"navbar-brand\" routerLink=\"/\" routerLinkActive=\"active\">Buy Movie Ticket</a>\n    </div>\n    <div id=\"navbar\" class=\"collapse navbar-collapse\">\n      <ul class=\"nav navbar-nav\">\n        <li><a routerLink=\"/\" routerLinkActive=\"active\">Home</a></li>\n        <li><a routerLink=\"/buyticket\" routerLinkActive=\"active\">Book ticket</a></li>\n      </ul>\n    </div>\n    <!--/.nav-collapse -->\n  </div>\n</nav>"
+
+/***/ }),
+
+/***/ 167:
+/***/ (function(module, exports) {
+
+module.exports = "<div class=\"container\">\n  <div style=\"position: static; height: 330px; width: 1400px;\">\n    <!--<div class=\"col-lg-1  col-centered\">-->\n    <div *ngFor=\"let row of settings.rows; index as i\">\n      <div class=\"btn-group\" role=\"group\" *ngFor=\"let col of settings.cols; index as j\">\n        <button type=\"button\" [ngClass]=\"getClassName(i,j)\" (click)=\"seatClick(getSeatNo(i,j), $event)\" class=\"btn btn-default\">\n      <!--<a title=\"{{getSeatNo(i,j)}}\">{{getSeatNo(i,j)}}</a>-->\n    </button>\n      </div>\n    </div>\n    <!--</div>-->\n  </div>\n  <div class=\"row\">\n\n    <button class=\"col-md-4\" type=\"button\" class=\"btn btn-primary\" (click)=\"bookTicket()\">Book ticket</button>\n    <span class=\"col-md-4\"></span>\n  </div>\n</div>\n<!--[style.top.px]=\"getTop(i)\" [style.left.px]=\"getLeft(j)\"-->"
+
+/***/ }),
+
+/***/ 200:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(92);
+
+
+/***/ }),
+
+/***/ 63:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = __webpack_require__(4);
+var CarouselComponent = (function () {
+    function CarouselComponent() {
+    }
+    CarouselComponent.prototype.ngOnInit = function () {
+    };
+    return CarouselComponent;
+}());
+CarouselComponent = __decorate([
+    core_1.Component({
+        selector: 'app-carousel',
+        template: __webpack_require__(165),
+        styles: [__webpack_require__(160)]
+    }),
+    __metadata("design:paramtypes", [])
+], CarouselComponent);
+exports.CarouselComponent = CarouselComponent;
+//# sourceMappingURL=carousel.component.js.map
+
+/***/ }),
+
+/***/ 64:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = __webpack_require__(4);
+var router_1 = __webpack_require__(62);
+var SeatlayoutComponent = (function () {
+    function SeatlayoutComponent(route) {
+        this.route = route;
+        this.reservedSeat = [];
+        this.selectingSeat = [];
+        this.settings = {
+            rows: [1, 1, 1, 1, 1],
+            cols: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            rowCssPrefix: 'row-',
+            colCssPrefix: 'col-',
+            seatWidth: 35,
+            seatHeight: 35,
+            seatCss: 'seat',
+            selectedSeatCss: 'selectedSeat',
+            selectingSeatCss: 'selectingSeat'
+        };
+        console.log("Getting settings", localStorage.getItem("settings"));
+        var ls_settings = localStorage.getItem("settings");
+        console.log(ls_settings);
+        if (ls_settings == null || ls_settings == "undefined") {
+            console.log("COMing INSIDE");
+            localStorage.setItem("settings", JSON.stringify(this.settings));
+        }
+        else {
+            if (typeof ls_settings === 'string') {
+                this.settings = JSON.parse(localStorage.getItem("settings"));
+            }
+        }
+        if (!localStorage.getItem("reservedSeat")) {
+            localStorage.setItem("reservedSeat", JSON.stringify(this.reservedSeat));
+        }
+        else {
+            this.reservedSeat = JSON.parse(localStorage.getItem("reservedSeat"));
+        }
+        // if (!localStorage.getItem("reservedSeat")) {
+        //   localStorage.setItem("reservedSeat", JSON.stringify(this.reservedSeat));
+        // } else {
+        //   this.reservedSeat = JSON.parse(localStorage.getItem("reservedSeat"));
+        // }
+        this.rowLength = this.settings.rows.length;
+    }
+    SeatlayoutComponent.prototype.getSeatNo = function (i, j) {
+        return (i + j * this.rowLength + 1);
+    };
+    SeatlayoutComponent.prototype.getClassName = function (i, j) {
+        var id = this.settings.rowCssPrefix + i.toString() + ' ' + this.settings.colCssPrefix + j.toString();
+        var className = this.settings.seatCss + ' ' + id;
+        if (this.reservedSeat.length > 0 && this.reservedSeat.indexOf(this.getSeatNo(i, j)) != -1) {
+            className += (' ' + this.settings.selectedSeatCss);
+        }
+        return className;
+    };
+    SeatlayoutComponent.prototype.seatClick = function (seatno, event) {
+        if (event.target.className.indexOf('selectedSeat') !== -1) {
+            return alert('Already booked! :(');
+        }
+        if (event.target.className.indexOf('selectingSeat') == -1) {
+            event.target.className += ' selectingSeat';
+            this.selectingSeat.push(seatno);
+        }
+        else {
+            event.target.className.replace('selectingSeat', '');
+            var index = this.selectingSeat.indexOf(seatno);
+            if (index > -1) {
+                this.selectingSeat.splice(index, 1);
+            }
+        }
+        console.log(event.target.className, seatno);
+    };
+    SeatlayoutComponent.prototype.getTop = function (i) {
+        return i * this.settings.seatHeight;
+    };
+    SeatlayoutComponent.prototype.getLeft = function (j) {
+        return j * this.settings.seatWidth;
+    };
+    SeatlayoutComponent.prototype.bookTicket = function () {
+        if (this.selectingSeat.length == 0) {
+            alert('Please select seat..!');
+            return;
+        }
+        this.reservedSeat = this.reservedSeat.concat(this.selectingSeat);
+        localStorage.setItem("reservedSeat", JSON.stringify(this.reservedSeat));
+        alert('You have booked your ticket successfully');
+        this.route.navigate(['/']);
+    };
+    SeatlayoutComponent.prototype.ngOnInit = function () {
+    };
+    return SeatlayoutComponent;
+}());
+SeatlayoutComponent = __decorate([
+    core_1.Component({
+        selector: 'app-seatlayout',
+        template: __webpack_require__(167),
+        styles: [__webpack_require__(162)]
+    }),
+    __metadata("design:paramtypes", [typeof (_a = typeof router_1.Router !== "undefined" && router_1.Router) === "function" && _a || Object])
+], SeatlayoutComponent);
+exports.SeatlayoutComponent = SeatlayoutComponent;
+var _a;
+//# sourceMappingURL=seatlayout.component.js.map
+
+/***/ }),
+
+/***/ 91:
+/***/ (function(module, exports) {
+
+function webpackEmptyContext(req) {
+	throw new Error("Cannot find module '" + req + "'.");
+}
+webpackEmptyContext.keys = function() { return []; };
+webpackEmptyContext.resolve = webpackEmptyContext;
+module.exports = webpackEmptyContext;
+webpackEmptyContext.id = 91;
+
+
+/***/ }),
+
+/***/ 92:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = __webpack_require__(4);
+var platform_browser_dynamic_1 = __webpack_require__(99);
+var app_module_1 = __webpack_require__(101);
+var environment_1 = __webpack_require__(104);
+if (environment_1.environment.production) {
+    core_1.enableProdMode();
+}
+platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
+//# sourceMappingURL=main.js.map
+
+/***/ })
+
+},[200]);
+//# sourceMappingURL=main.bundle.js.map
